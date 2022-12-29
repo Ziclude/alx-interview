f3938a16e6a272f586f2509fe8f7c618a69b47f3
ziclude <cindyzickziemi@gmail.com>
2022-12-29 13:40:30 -0800

    cleanup function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,13 +10,18 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    output = [[1]]
+    output = [0] * n
 
-    for a in range(n - 1):
-        tp = [0] + output[-1]+[0]
-        rw = []
-        for b in range(len(output[-1]) + 1):
-            rw.append(tp[b] + tp[b + 1])
-            output.append(rw)
+    for a in range(n):
+        tp = [0] * (a+1)
+        tp[0] = 1
+        tp[len(tp) - 1] = 1
+        
+        for b in range(1, a):
+            if b > 0 and b < len(tp):
+                i = output[a - 1][b]
+                j = output[a - 1][b - 1]
+                tp[b] = i + j
+        output[a] = tp
 
     return (output)
